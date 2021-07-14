d5a7065d356e22944d21cc8d79e4c43a6ebfdfc7
Remomare <75660801+Remomare@users.noreply.github.com>
2021-07-15 02:27:43 +0900

    Create lstm_padding.py

---
+++ lstm_padding.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def padding_(sentences, seq_len):
+    features = np.zeros((len(sentences), seq_len),dtype=int)
+    for ii, review in enumerate(sentences):
+        if len(review) != 0:
+            features[ii, -len(review):] = np.array(review)[:seq_len]
+    return features
