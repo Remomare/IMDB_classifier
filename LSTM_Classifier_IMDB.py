9a2d2e62b1a3139d2708fb7b452614a1c0223e6a
Remomare <75660801+Remomare@users.noreply.github.com>
2021-07-15 03:10:35 +0900

    Update LSTM_Classifier_IMDB.py

--- LSTM_Classifier_IMDB.py
+++ LSTM_Classifier_IMDB.py
@@ -1,22 +1,20 @@
-from sys import platform
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import string
-import seaborn as sns
-from tqdm import tqdm
-import matplotlib.pyplot as plt
 from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import train_test_split
 import device_set
 import tockenization as tockenize
 import lstm_padding as padding_
+import lstm_model as SentimentRNN
+import accuracy as accuracy_
+import inferance as predict_text
+import criterion as BCELoss
+import optimizer as Adam
 
 #Cpu or Gpu
-device = device_set
-print(device)
+print(device_set)
 
 base_csv = '/kaggle/input/imdb-dataset-of-50k-movie-reviews/IMDB Dataset.csv'
 df = pd.read_csv(base_csv)
@@ -24,12 +22,6 @@ df.head()
 
 X,y = df['review'].values,df['sentiment'].values
 x_train,x_test,y_train,y_test = train_test_split(X,y,stratify=y)
-print(f'shape of train data is {x_train.shape}')
-print(f'shape of test data is {x_test.shape}')
-
-dd = pd.Series(y_train).value_counts()
-sns.barplot(x=np.array(['negative','positive']),y=dd.values)
-plt.show()
 
 #tockenize
 x_train,y_train,x_test,y_test,vocab = tockenize(x_train,y_train,x_test,y_test)
@@ -50,4 +42,112 @@ batch_size = 50
 train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
 valid_loader = DataLoader(valid_data, shuffle=True, batch_size=batch_size)
 
+no_layers = 2
+vocab_size = len(vocab) + 1 #extra 1 for padding
+embedding_dim = 64
+output_dim = 1
+hidden_dim = 256
 
+#model set
+model = SentimentRNN(no_layers,vocab_size,output_dim,hidden_dim,embedding_dim,drop_prob=0.5)
+
+#moving to gpu
+model.to(device_set)
+print(model)
+
+# loss and optimization functions
+lr=0.001
+
+criterion = BCELoss()
+
+optimizer = Adam(model.parameters(), lr)
+
+clip = 5
+epochs = 5 
+valid_loss_min = np.Inf
+# train for some number of epochs
+epoch_tr_loss,epoch_vl_loss = [],[]
+epoch_tr_acc,epoch_vl_acc = [],[]
+
+for epoch in range(epochs):
+    train_losses = []
+    train_acc = 0.0
+    model.train()
+    # initialize hidden state 
+    h = model.init_hidden(batch_size)
+    for inputs, labels in train_loader:
+        
+        inputs, labels = inputs.to(device_set), labels.to(device_set)   
+        # Creating new variables for the hidden state, otherwise
+        # we'd backprop through the entire training history
+        h = tuple([each.data for each in h])
+        
+        model.zero_grad()
+        output,h = model(inputs,h)
+        
+        # calculate the loss and perform backprop
+        loss = criterion(output.squeeze(), labels.float())
+        loss.backward()
+        train_losses.append(loss.item())
+        # calculating accuracy
+        accuracy = accuracy_(output,labels)
+        train_acc += accuracy
+        #`clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
+ 
+    
+        
+    val_h = model.init_hidden(batch_size)
+    val_losses = []
+    val_acc = 0.0
+    model.eval()
+    for inputs, labels in valid_loader:
+            val_h = tuple([each.data for each in val_h])
+
+            inputs, labels = inputs.to(device_set), labels.to(device_set)
+
+            output, val_h = model(inputs, val_h)
+            val_loss = criterion(output.squeeze(), labels.float())
+
+            val_losses.append(val_loss.item())
+            
+            accuracy = accuracy_(output,labels)
+            val_acc += accuracy
+            
+    epoch_train_loss = np.mean(train_losses)
+    epoch_val_loss = np.mean(val_losses)
+    epoch_train_acc = train_acc/len(train_loader.dataset)
+    epoch_val_acc = val_acc/len(valid_loader.dataset)
+    epoch_tr_loss.append(epoch_train_loss)
+    epoch_vl_loss.append(epoch_val_loss)
+    epoch_tr_acc.append(epoch_train_acc)
+    epoch_vl_acc.append(epoch_val_acc)
+    print(f'Epoch {epoch+1}') 
+    print(f'train_loss : {epoch_train_loss} val_loss : {epoch_val_loss}')
+    print(f'train_accuracy : {epoch_train_acc*100} val_accuracy : {epoch_val_acc*100}')
+    if epoch_val_loss <= valid_loss_min:
+        torch.save(model.state_dict(), '../working/state_dict.pt')
+        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min,epoch_val_loss))
+        valid_loss_min = epoch_val_loss
+    print(25*'==')
+
+index = 30
+print(df['review'][index])
+print('='*70)
+print(f'Actual sentiment is  : {df["sentiment"][index]}')
+print('='*70)
+pro = predict_text(df['review'][index],model,vocab)
+status = "positive" if pro > 0.5 else "negative"
+pro = (1 - pro) if status == "negative" else pro
+print(f'Predicted sentiment is {status} with a probability of {pro}')
+
+index = 32
+print(df['review'][index])
+print('='*70)
+print(f'Actual sentiment is  : {df["sentiment"][index]}')
+print('='*70)
+pro = predict_text(df['review'][index],model,vocab)
+status = "positive" if pro > 0.5 else "negative"
+pro = (1 - pro) if status == "negative" else pro
+print(f'predicted sentiment is {status} with a probability of {pro}')
